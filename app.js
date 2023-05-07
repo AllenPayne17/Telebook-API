504cec088bef88bf82ffc7ed46cc2d1eae8d1051
allen spayne <mainacc.salapayne@gmail.com>
2023-05-07 11:53:40 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const registrationSchema = new mongoose.Schema({
+  name: String,
+  contactNumber: Number,
+  email: String,
+  date: Date,
+});
+
+const Registration = mongoose.model('Registration', registrationSchema);
+
+app.post('/register', async (req, res) => {
+  const registration = new Registration({
+    name: req.body.name,
+    contactNumber: req.body.contactNumber,
+    email: req.body.email,
+    date: req.body.date,
+  });
+  
+
+  try {
+    await registration.save();
+    res.status(201).send('Registration successful');
+  } catch (error) {
+    res.status(500).send('Error registering user');
+  }
+});
+
+app.get('/registrations', async (req, res) => {
+    try {
+      const registrations = await Registration.find();
+      res.status(200).json(registrations);
+    } catch (error) {
+      res.status(500).send('Error fetching registrations');
+    }
+  });
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
